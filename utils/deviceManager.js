7713f6b577a41595d774000c06530cba6cc3d1a6
hwq <hwq@qq.com>
2026-01-12 15:47:45 +0800

    原先每100毫秒请求一次实时数据改为每秒请求一次

--- utils/deviceManager.js
+++ utils/deviceManager.js
@@ -643,7 +643,7 @@ voiceNotifation(params){
     this.clearRealtimeTimer();
     this._realtimeTimer = setInterval(() => {
       this.getDeviceRealtimeData(mac);
-    }, 100);
+    }, 1000);
     this.page.setData({ _realtimeTimer: this._realtimeTimer });
   }
 
